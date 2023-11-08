2c609b53ff16e1b1c86088bd9ba6926e0fa43a5d
akshaybidwai09 <abidwai@scu.edu>
2023-11-07 18:09:43 -0800

    Adding API Integration for Logn-Registraion flow

--- src/components/UserRegistration.tsx
+++ src/components/UserRegistration.tsx
@@ -1,80 +1,108 @@
-// src/components/UserRegistration.tsx
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom"; // Import useHistory
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 type UserRegistrationData = {
-  name: string;
-  surname: string;
-  dob: string; // date of birth
+  firstName: string;
+  lastName: string;
+  dob: string;
   email: string;
   password: string;
 };
 
 const UserRegistration: React.FC = () => {
   const [userData, setUserData] = useState<UserRegistrationData>({
-    name: "",
-    surname: "",
+    firstName: "",
+    lastName: "",
     dob: "",
     email: "",
     password: "",
   });
-  const history = useHistory(); // Initialize useHistory
+  const [message, setMessage] = useState("");
+  const history = useHistory();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(userData);
-    history.push("/main"); // Navigate to the main page after registration
+    setMessage("");
+    const formattedDob =
+      new Date(userData.dob).toISOString().split("T")[0] + "T00:00:00Z";
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8080/api/auth/register",
+        {
+          ...userData,
+          dob: formattedDob,
+        }
+      );
+      if (response.data.statusCode === 200) {
+        localStorage.setItem(
+          "userProfile",
+          JSON.stringify(response.data.response)
+        );
+        history.push("/main");
+      } else {
+        setMessage(response.data.error);
+      }
+    } catch (error: any) {
+      setMessage(
+        error.response?.data?.error || "An error occurred during registration."
+      );
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="name"
-        value={userData.name}
-        onChange={handleInputChange}
-        placeholder="Name"
-        required
-      />
-      <input
-        type="text"
-        name="surname"
-        value={userData.surname}
-        onChange={handleInputChange}
-        placeholder="Surname"
-        required
-      />
-      <input
-        type="date"
-        name="dob"
-        value={userData.dob}
-        onChange={handleInputChange}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        value={userData.email}
-        onChange={handleInputChange}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        value={userData.password}
-        onChange={handleInputChange}
-        placeholder="Password"
-        required
-      />
-      <button className="form-button" type="submit">
-        Register
-      </button>
-    </form>
+    <div>
+      {message && <div>{message}</div>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          value={userData.firstName}
+          onChange={handleInputChange}
+          placeholder="First Name"
+          required
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={userData.lastName}
+          onChange={handleInputChange}
+          placeholder="Last Name"
+          required
+        />
+        <input
+          type="date"
+          name="dob"
+          value={userData.dob}
+          onChange={handleInputChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          value={userData.email}
+          onChange={handleInputChange}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={userData.password}
+          onChange={handleInputChange}
+          placeholder="Password"
+          required
+        />
+        <button className="form-button" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
   );
 };
 
